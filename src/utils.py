5b7507addc427bfb1af08f9eec9d2d291fb78430
Chufei Luo <33498986+chufeiluo@users.noreply.github.com>
2025-07-15 10:16:05 -0400

    renaming prompt_chain.py to utils.py, added some functions for processing data

---
+++ src/utils.py
@@ -0,0 +1,47 @@
+import json
+from flowchart import Flowchart, FlowchartTask, FlowchartTaskResult
+
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+
+def create_test(input_file, file_format='jsonl', split=True, test_file=''):
+    if split: # split doesnt exist, make from input_file
+        pass
+    else: # split alr exists, read input file as train and a separate file as test
+        pass
+
+def google_form(results):
+    return [f'Title: {x['title']}\n\nSnippet: {x['snippet']}' for x in results]
+
+OUTPUT_FORMAT_MAPPING = {
+    'google.com': google_form, 
+}
+
+EVAL_METRICS = {
+    'example.json': ['bleu']
+}
+
+llm = None
+
+def init(model_name, provider, t=0.3 ):
+    global llm
+    llm = init_chat_model(
+            model_name,
+            model_provider=provider,
+            temperature=t)
+
+def FlowchartTask_llm_execution(node: FlowchartTask, input: str) -> FlowchartTaskResult:
+    """
+    Execute a FlowchartNode and return the output.
+    This function simulates the execution of a flowchart node using a basic LLM call.
+
+    currently, the audit is just the prompt and the response from the LLM.
+    """
+    assert isinstance(
+        node, FlowchartTask), "node must be a FlowchartTask instance"
+    messages = [
+        SystemMessage(content="You are a helpful assistant."),
+        HumanMessage(content=node.to_prompt(input)),
+    ]
+    response = llm.invoke(messages)
+    return FlowchartTaskResult(value=response.content, executionDetails={"promptMessages": messages, "response": response})
