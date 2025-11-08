d65b68b08f5a08d2c9fa58e04c2c37c2dcf60223
Kevin Wang <kayhne999@gmail.com>
2025-11-08 01:45:04 -0500

    Adding RAG metrics to benchmarking

--- src/batch.bash
+++ src/batch.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
-bash ../activate/Scripts/activate;
-python benchmarking.py -m gpt-4o-mini -i data/cd.jsonl -f flowcharts/rag.json -o 'data/results' -k 10
+# bash ../activate/Scripts/activate;
+# python benchmarking.py -m gpt-4o-mini -i data/cd.jsonl -f flowcharts/rag.json -o 'data/results' -k 10
+source ../.venv/Scripts/activate
+python benchmarking.py -m gpt-4o-mini -i data/cd2.jsonl -f flowcharts/updated_rag.json -o 'data/results' -k 2
