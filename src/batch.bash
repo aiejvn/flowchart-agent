f9ce6f9a0d1182f1c4d735bc66e2cace06effbfd
Kevin Wang <kayhne999@gmail.com>
2025-11-11 20:54:49 -0500

    Cleaning up comments, fixing up requirements.txt

--- src/batch.bash
+++ src/batch.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-# bash ../activate/Scripts/activate;
-# python benchmarking.py -m gpt-4o-mini -i data/cd.jsonl -f flowcharts/rag.json -o 'data/results' -k 10
-source ../.venv/Scripts/activate
-python benchmarking.py -m gpt-4o-mini -i data/cd2.jsonl -f flowcharts/updated_rag.json -o 'data/results' -k 2
+
+bash ../activate/Scripts/activate;
+python benchmarking.py -m gpt-4o-mini -i data/cd.jsonl -f flowcharts/updated_rag.json -o 'data/results' -k 10
